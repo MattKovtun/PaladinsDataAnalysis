5a3af5e6284a62e078ad3cc434be370bdb402833
mkovtun <kovtun_m@ucu.edu.ua>
2019-11-27 12:02:42 +0200

    code cleaning

--- src/match_scrapper.py
+++ src/match_scrapper.py
@@ -7,15 +7,15 @@ from datetime import timedelta, datetime
 
 from src.dev_creds import DEV_ID, AUTH_KEY
 from src.paladins_api.match_api import MatchApi
-from src.constants import SIEGE, TIER_N, DATA_DAILY, RANKED_SIEGE
+from src.constants import RANKED_SIEGE
 from src.paladins_api.match import Match
 
 
 def scrap_ranked_matches():
     api = MatchApi(DEV_ID, AUTH_KEY)
-    today = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
-    data = api.get_match_ids_by_queue(RANKED_SIEGE, today, hour='-1', verbose=False)
-    pp = '../data/daily/ranked_siege/' + today
+    yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
+    data = api.get_match_ids_by_queue(RANKED_SIEGE, yesterday, hour='-1', verbose=False)
+    pp = '../data/daily/ranked_siege/' + yesterday
 
     with open(pp, 'w') as f:
         f.write(str(len(data)) + '\n')
@@ -23,11 +23,12 @@ def scrap_ranked_matches():
             f.write(line['Match'] + '\n')
 
 
-def match_to_tier(match_path, n_matches=20):
+def matches_to_tiers(matches_id, n_matches=20):
+    matches_id_path = '../data/daily/ranked_siege/' + matches_id
     tier_path = '../data/tiers/'
 
     match_ids = []
-    with open(match_path) as input_file:
+    with open(matches_id_path) as input_file:
         input_file.readline()
         for line in input_file:
             line = line.strip()
@@ -49,6 +50,7 @@ def match_to_tier(match_path, n_matches=20):
 
 
 if __name__ == "__main__":
-    match_path = '../data/daily/ranked_siege/20191124'
-    match_to_tier(match_path, 10)
     # scrap_ranked_matches()
+    # this file has to be present in data/daily
+    matches_id = '20191126'
+    matches_to_tiers(matches_id, 1)
