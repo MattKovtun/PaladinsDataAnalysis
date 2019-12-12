eebe618850ecd1097d57f3f00674ae665682f8ce
mkovtun <kovtun_m@ucu.edu.ua>
2019-12-12 14:23:44 +0200

    small changes

--- src/app.py
+++ src/app.py
@@ -59,7 +59,7 @@ hero_drop_down_callback(app, DEFAULT_HERO)
 observations_callback(app, NUMBER_OF_BANS, TIERS, global_store)
 
 if __name__ == '__main__':
-    app.run_server(host='0.0.0.0', debug=True)
+    app.run_server(host='0.0.0.0', debug=False)
 
 # refactor code
 # add date selection
